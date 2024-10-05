83089179dbe639e01918df94b3ccf5d3e6f3bd08
augustosnk12 <augustosnk12@gmail.com>
2024-10-05 10:00:38 -0300

    fix: add font-family to cardInformation

--- src/app/components/cardInformation.tsx
+++ src/app/components/cardInformation.tsx
@@ -2,11 +2,17 @@
 import React from "react";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import { useGeomapContext } from "../contexts/GeomapContext";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export default function CardInformation() {
   const { selectedPlace, setOpenPlaceInfoModal } = useGeomapContext();
   return (
-    <div className="relative bg-black/50 max-w-screen h-screen mx-auto z-[1000]">
+    <div className={`relative bg-black/50 max-w-screen h-screen mx-auto z-[1000] ${poppins.className}`}>
       <div className="relative h-[80vh] max-w-3xl inset-y-16 mx-auto bg-white rounded-xl shadow-lg overflow-hidden z-[999]">
         <button
           className="absolute top-2 right-2 text-white rounded-full p-1 hover:bg-red-500 transition"
@@ -22,11 +28,11 @@ export default function CardInformation() {
         />
 
         <div className="p-6">
-          <h2 className="text-xl font-bold">{selectedPlace.razao_social}</h2>
-          <p className="text-gray-700  mb-2">{selectedPlace.nome_fantasia}</p>
-          <p className="text-gray-700">{`${selectedPlace.logradouro}, 
+          <div className="text-xl font-bold">{selectedPlace.razao_social}</div>
+          <div className="text-gray-700  mb-2">{selectedPlace.nome_fantasia}</div>
+          <div className="text-gray-700">{`${selectedPlace.logradouro}, 
           ${selectedPlace.numero}, ${selectedPlace.bairro}, 
-          ${selectedPlace.cidade}-${selectedPlace.uf}`}</p>
+          ${selectedPlace.cidade}-${selectedPlace.uf}`}</div>
           <p className="text-gray-700">{selectedPlace.telefone}</p>
         </div>
 
